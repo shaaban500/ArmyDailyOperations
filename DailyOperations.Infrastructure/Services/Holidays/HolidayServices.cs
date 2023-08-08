55ee64823db2aa50ec1491750b7f63d8f4f7c227
Mohamed Shaaban <moshaaban500@gmail.com>
2023-08-08 06:40:34 +0300

    fix IsMemberInHoliday methods

--- DailyOperations.Infrastructure/Services/Holidays/HolidayServices.cs
+++ DailyOperations.Infrastructure/Services/Holidays/HolidayServices.cs
@@ -14,17 +14,18 @@ namespace DailyOperations.Infrastructure.Services.Holidays
         public async Task<bool> IsSoldierInHoliday(long id, DateTime operationDate)
         {
             var soldierHoliday = await _unitOfWork.SoldierHolidays.GetByIdAsync(x =>
-                                        !x.IsFinished &&
+                                        x.IsFinished != true &&
                                         x.HolidayStartDate <= operationDate &&
                                         x.HolidayEndDate >= operationDate &&
                                         x.SoldierId == id);
 
             return soldierHoliday != null;
         }
-        public async Task<bool> IsAssistantInHoliday(long id, DateTime operationDate)
+
+        public async Task<bool> IsOfficerInHoliday(long id, DateTime operationDate)
         {
             var officerHoliday = await _unitOfWork.OfficerHolidays.GetByIdAsync(x =>
-                                        !x.IsFinished &&
+                                        x.IsFinished != true &&
                                         x.HolidayStartDate <= operationDate &&
                                         x.HolidayEndDate >= operationDate &&
                                         x.PoliceOfficerId == id);
@@ -32,10 +33,10 @@ namespace DailyOperations.Infrastructure.Services.Holidays
             return officerHoliday != null;
         }
 
-        public async Task<bool> IsOfficerInHoliday(long id, DateTime operationDate)
+        public async Task<bool> IsAssistantInHoliday(long id, DateTime operationDate)
         {
             var assistantHoliday = await _unitOfWork.AssistantHolidays.GetByIdAsync(x =>
-                                        !x.IsFinished &&
+                                        x.IsFinished != true &&
                                         x.HolidayStartDate <= operationDate &&
                                         x.HolidayEndDate >= operationDate &&
                                         x.PoliceAssistantId == id);
